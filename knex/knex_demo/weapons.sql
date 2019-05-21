72d17e113d1af952dab0fbcb6bee90786cbefc1f
sadief <sadiefreeman@gmail.com>
2019-05-20 23:19:38 -0700

    Revert to original for demo

--- knex/knex_demo/weapons.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE weapons (
-    weapon text,
-    character_id bigint references characters(id)
-);
-
-insert into weapons (weapon, character_id) values ('Sword', 1);
-insert into weapons (weapon, character_id) values ('Face Blindness', 3);
-insert into weapons (weapon, character_id) values ('Snark', 2);
-insert into weapons (weapon, character_id) values ('Sword', 5);
